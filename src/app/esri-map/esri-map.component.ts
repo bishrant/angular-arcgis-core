07d9ac5c9547d2d144cf5ef0f61894b66e3a6bc9
Bishrant Adhikariu <bishrant.adhikari@tfs.tamu.edu>
2021-05-04 15:09:49 -0500

    Initial commit

---
+++ src/app/esri-map/esri-map.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import MapView from '@arcgis/core/views/MapView';
+import Map from '@arcgis/core/Map';
+
+@Component({
+  selector: 'app-esri-map',
+  templateUrl: './esri-map.component.html',
+  styleUrls: ['./esri-map.component.scss']
+})
+export class EsriMapComponent implements AfterViewInit {
+  esriMap: Map | undefined;
+  mapView: MapView | undefined;
+
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef | undefined;
+  constructor() { }
+
+  
+  initMap = async () => {
+    this.esriMap = new Map({
+      basemap: 'streets'
+    })
+    const mapViewProperties = {
+      container: this.mapViewEl?.nativeElement,
+      center: [0.1278, 51.5074],
+      zoom: 8,
+      map: this.esriMap
+    };
+
+    this.mapView = new MapView(mapViewProperties);
+
+    await this.mapView.when(); 
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+}
